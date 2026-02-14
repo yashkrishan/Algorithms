67428ed54df4ea992d53481f76f5adc76f1efdce
potpie-codegen <codegen@potpie.ai>
2026-02-14 16:08:55 +0530

    codegen: Discovery and File Setup - Create bubble sort.cpp file struc...

---
+++ Array/bubble sort.cpp
@@ -0,0 +1,16 @@
+// bubble sort
+/*
+Given an array of integers, sort the array using bubble sort algorithm and return the sorted array.
+
+Example:
+Given [4, 3, 2, 1]
+Return [1, 2, 3, 4]
+*/
+
+vector<int> Solution::bubbleSort(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+}
